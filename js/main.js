6376514352bebbe1c2ed1694c9d5fdbea3c01912
Sarad Singh <thearkein@Sarads-MacBook-Air.local>
2021-07-27 21:21:14 +0545

    search clients functionality

--- js/main.js
+++ js/main.js
@@ -4,25 +4,26 @@
 
     // fetch clients
     let fetchClients = async () => {
-        let response = await fetch(url)
+        let response = await fetch(url);
         if (response.status != 200) {
-            window.alert("Something went wrong with api. Please contact admin")
+            window.alert("Something went wrong with api. Please contact admin");
         }
-        let result = await response.json()
+        let result = await response.json();
         if (result.error) {
-            window.alert("Something went wrong with api. Please contact admin")
+            window.alert("Something went wrong with api. Please contact admin");
         }
-        clients = result.data
+        clients = result.data;
         clients.forEach((client, index) => {
-            client.id = index + 1
+            client.id = index + 1;
         });
-        console.log(clients)
-        displayClientCards(clients)
+        console.log(clients);
+        displayClientCards(clients);
     }
 
+    // display client cards in page
     let displayClientCards = (clients) => {
         if (!clients.length) {
-            document.querySelector('.cards').innerHTML = "No clients found in database. Add one"
+            document.querySelector('.cards').innerHTML = "No clients found in database. Add one";
         }
         document.querySelector('.cards').innerHTML = "";
         clients.forEach(client => {
@@ -33,12 +34,29 @@
                     <div class="name">${client.firstname} ${client.lastname}</div>
                     <button class="view-client-detail-btn" client-target="${client.id}" href="#" >View Details</button>
                 </div>
-            </div>`
-            let container = document.querySelector('.cards')
-            container.innerHTML = container.innerHTML + card
-
+            </div>`;
+            let container = document.querySelector('.cards');
+            container.innerHTML = container.innerHTML + card;
         });
     }
+
+    // functionality for client search
+    let searchbox = document.querySelector('.searchbar input');
+    searchbox.addEventListener('keyup', (event) => {
+        let matchedClients = [];
+        let query = document.querySelector('.searchbar input').value;
+        clients.forEach(client => {
+            let name = (client.firstname + " " + client.lastname).toLowerCase();
+            if (name.includes(query.toLowerCase())) {
+                matchedClients.push(client);
+            }
+        })
+        if (!matchedClients.length) {
+            document.querySelector('.cards').innerHTML = "Search result: No such clients found"
+        } else {
+            displayClientCards(matchedClients)
+        }
+    })
 
 
     await fetchClients()
