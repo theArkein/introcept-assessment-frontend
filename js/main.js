e2ed91754c46a74a45435e4f93f4ba6d9e92534e
Sarad Singh <thearkein@Sarads-MacBook-Air.local>
2021-07-27 21:43:12 +0545

    see client details ui & functionality

--- js/main.js
+++ js/main.js
@@ -26,18 +26,31 @@
             document.querySelector('.cards').innerHTML = "No clients found in database. Add one";
         }
         document.querySelector('.cards').innerHTML = "";
+
+        // render client card ui 
         clients.forEach(client => {
             let card = `
             <div class="card">
                 <div class="left">
                     <div class="icon">${client.firstname[0]}</div>
                     <div class="name">${client.firstname} ${client.lastname}</div>
-                    <button class="view-client-detail-btn" client-target="${client.id}" href="#" >View Details</button>
+                    <button class="view-client-detail-btn" data-target="${client.id}" href="#" >View Details</button>
                 </div>
             </div>`;
             let container = document.querySelector('.cards');
             container.innerHTML = container.innerHTML + card;
         });
+
+        // show client detail view on view details button click for targeted client
+        let btns = document.querySelectorAll('.view-client-detail-btn')
+        btns.forEach(btn => {
+            let target = btn.getAttribute('data-target')
+            btn.addEventListener('click', () => {
+                let detailView = document.querySelector('.view-client-detail-wrapper')
+                setClientDetails(target)
+                detailView.style.display = "flex"
+            })
+        })
     }
 
     // functionality for client search
@@ -52,12 +65,34 @@
             }
         })
         if (!matchedClients.length) {
-            document.querySelector('.cards').innerHTML = "Search result: No such clients found"
+            document.querySelector('.cards').innerHTML = "Search result: No such clients found";
         } else {
-            displayClientCards(matchedClients)
+            displayClientCards(matchedClients);
         }
     })
 
+    // change data on client detail view for targeted client 
+    let setClientDetails = (target) => {
+        let client = clients[target - 1];
+        document.querySelector('.client-detail-card .icon').innerHTML = client.firstname[0];
+        document.querySelector('.client-detail-card .name').innerHTML = client.firstname + " " + client.lastname;
+        document.querySelector('.client-detail-card .details').innerHTML = `
+            Gender: ${client.gender}<br>
+            Dob: ${client.dob}<br>
+            Nationlaity: ${client.nationality}<br>
+            Email: ${client.email}<br>
+            Address: ${client.address}<br>
+            Education Level: ${client.educationLevel}<br>
+            Preferred mode of contact: ${client.contactMode}
+        `;
+    }
+
+    // hide client detail view on close button click
+    let btn = document.querySelector('.close-btn');
+    btn.addEventListener('click', () => {
+        let clientDetailViewWrapper = document.querySelector('.view-client-detail-wrapper');
+        clientDetailViewWrapper.style.display = "none";
+    })
 
     await fetchClients()
 })()
